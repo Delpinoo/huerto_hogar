786f71a95f3e31cc172e98b1b5731054602f088d
benjvmin01 <ben.monsalve@duocuc.cl>
2025-09-13 20:26:56 -0300

    Welcome_page_lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:huerto_hogar/pages/presentation/login_page.dart';
+import 'package:huerto_hogar/pages/presentation/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage());
+      home: WelcomePage());
   }
 }
